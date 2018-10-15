4654108e5d119ecbc8ca52180f9a10389c1618bb
Daniel choi <choiwaim@gmail.com>
2018-10-15 15:21:02 +1100

    init

---
+++ React/Scripts/ReactScript/webpack.config.js
@@ -0,0 +1,32 @@
+﻿module.exports = {
+    context: __dirname,
+    mode: 'development',
+    entry: {
+        customers: "./Customer.js"
+    }
+    ,
+    output: {
+        path: __dirname + "/dist",
+        filename: "[name].bundle.js"
+    }
+    ,
+    watch: true,
+    resolve: {
+        extensions: [".jsx", ".js"]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /(node_models)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ["@babel/preset-env", "@babel/preset-react"]
+                    }
+                }
+            }
+
+        ]
+    }
+};
